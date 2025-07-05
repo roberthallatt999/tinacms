fa6b5152d492d73c66882e6880e75e23148ffbe1
Robert Hallatt <roberth@cps.ca>
2025-07-05 17:48:01 -0400

    Update tiinacms

--- .tina/config.cjs
+++
@@ -1,6 +0,0 @@
-// .tina/config.cjs
-module.exports = {
-  publicUrl: 'http://localhost:3000',
-  localGraphQLURL: 'http://localhost:4001/graphql',
-  // Add any other TinaCMS CLI specific configs here
-};
